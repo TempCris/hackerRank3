10b0a4995f74b690c4446397f09bf26d2b17d7c5
kross <cristemperatum@gmail.com>
2021-12-12 16:14:43 -0600

    ejercicio 3

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,20 @@
 
 // Source: https://www.hackerrank.com/challenges/simple-array-sum/problem?isFullScreen=true
 
-function simpleArraySum(ar: Array<number>): number {
-  let suma = 0;
-  ar.forEach((element) => suma += element);
-  return suma;
+function aVeryBigSum( ar: number[] ) {
+  const longitud = ar.length 
+  let  sum= 0
+  for (let i = 0; i < longitud; i++) {
+    sum += ar[i]; 
+  }
+  
+  return sum;
 }
 
 function main() {
   console.log('---------- Cool Programing ----------\n');
-  const testArray = [1, 2, 3, 4, 10, 11];
-  const result = simpleArraySum(testArray);
+  const testArray = [5,1000000001, 1000000002, 1000000003, 1000000004, 1000000005];
+  const result = aVeryBigSum(testArray);
   console.log('Result: ', result);
 }
 
